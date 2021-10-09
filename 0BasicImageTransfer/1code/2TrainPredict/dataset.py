ca139b20a9298ef386ce8cfa3c8f7656802ed4fc
SichaoY <56497710+Sichao-Yang@users.noreply.github.com>
2021-10-09 15:56:45 +0800

    Update dataset.py

--- 0BasicImageTransfer/1code/2TrainPredict/dataset.py
+++ 0BasicImageTransfer/1code/2TrainPredict/dataset.py
@@ -1,5 +1,5 @@
 from os.path import join, listdir
-from dataset import DatasetFromFolder
+# from dataset import DatasetFromFolder
 
 def get_training_set(root_dir, direction):
     train_dir = join(root_dir, "train")
